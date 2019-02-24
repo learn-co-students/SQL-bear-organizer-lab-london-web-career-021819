50af77bc46f913af404388ad6c3c93362c721a20
Alex Bujenita <dekc89@gmail.com>
2019-02-24 21:27:09 +0000

    SQL queries

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 20, "Male", "dark brown", "calm", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 10, "Male", "black", "intense", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 6, "Female", "dark brown", "Nice", 0);
+INSERT INTO bears VALUES (4, "Sargeant Brown", 19, "Male", "Green", "Slimy", 1);
+INSERT INTO bears VALUES (5, "Melissa", 13, "Female", "dark brown", "goofy", 1);
+INSERT INTO bears VALUES (6, "Grinch", 2, "Male", "Black", "Grinchy", 1);
+INSERT INTO bears VALUES (7, "Wendy", 12, "Female", "Pink", "naive", 0);
+INSERT INTO bears VALUES (8, null, 20, "Male", "Black", "aggressive", 0);
